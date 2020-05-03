785158a85e39982d7647e0918a4972344b0c40a6
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-05-03 13:47:07 -0400

    VsCode

---
+++ vscode.js
@@ -0,0 +1,15 @@
+{
+    "workbench.iconTheme": "material-icon-theme",
+    "workbench.colorTheme": "Dracula",
+    "workbench.preferredDarkColorTheme": "soft era",
+    "workbench.preferredHighContrastColorTheme": "soft era",
+    "workbench.preferredLightColorTheme": "soft era",
+    "editor.fontSize": 22,
+    "workbench.sideBar.location": "right",
+    "editor.minimap.enabled": false,
+    "editor.formatOnSave": true,
+    "emmet.triggerExpansionOnTab": true,
+    "emmet.includeLanguages": {
+        "javascript": "javascriptreact"
+    },
+}
